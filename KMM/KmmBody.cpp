e32af3d6045ae3bd5f449ca721c6c948b176249b
Nat Moore <natalie@natnat.xyz>
2022-09-01 21:10:27 -0500

    more pain

---
+++ KMM/KmmBody.cpp
@@ -0,0 +1,10 @@
+#include "KmmBody.h"
+
+namespace kmm {
+    KmmBody::KmmBody() :
+    m_messageId((MessageID) 0),
+    m_inventoryType((InventoryType) 0),
+    m_responseKind((ResponseKind) 0)
+    {
+    }
+}
