0278d835e8fbc939ff81268ac37d975e8bbc7d85
Nat Moore <natalie@natnat.xyz>
2022-09-01 21:05:02 -0500

    p a i n

---
+++ KMM/KmmBody.h
@@ -0,0 +1,21 @@
+#if !defined(__KmmBody_H__)
+#define __KmmBody_H__
+
+#include <stdint.h>
+#include "MessageID.h"
+#include "ResponseKind.h"
+#include "KMM/InventoryCommand/InventoryType.h"
+
+namespace kmm
+{
+    class KmmBody {
+    public:
+        KmmBody();
+        MessageID m_messageId;
+        InventoryType m_inventoryType;
+        ResponseKind m_responseKind;
+        uint8_t toBytes(uint8_t *contents);
+    };
+}
+
+#endif
---
+++ KMM/KmmFrame.h
@@ -0,0 +1,23 @@
+#if !defined(__ListKmfRsi_H__)
+#define __ListKmfRsi_H__
+
+#include <stdint.h>
+#include "KMM/MessageID.h"
+#include "KMM/ResponseKind.h"
+#include "InventoryType.h"
+
+namespace kmm
+{
+    class InventoryCommandListKmfRsi {
+    public:
+        InventoryCommandListKmfRsi();
+        MessageID m_messageId;
+        InventoryType m_inventoryType;
+        ResponseKind m_responseKind;
+        uint8_t toBytes(uint8_t *contents);
+    private:
+        uint16_t m_frameLen;
+    };
+}
+
+#endif
