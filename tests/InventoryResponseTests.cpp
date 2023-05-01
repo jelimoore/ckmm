47f9964663313407279e3681169bbc6806effdf4
Natalie Moore <natalie@natnat.xyz>
2023-05-01 15:03:50 +0000

    commit before pull

---
+++ tests/InventoryResponseTests.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../src/InventoryCommand/InventoryType.h"
+#include "../src/InventoryCommand/ListActiveKeys.h"
+#include "../src/InventoryCommand/ListActiveKeysetIds.h"
+#include "../src/InventoryCommand/ListKeysetTaggingInfo.h"
+#include "../src/InventoryCommand/ListKmfRsi.h"
+#include "../src/InventoryCommand/ListMnp.h"
+#include "../src/InventoryCommand/ListRsiItems.h"
+
